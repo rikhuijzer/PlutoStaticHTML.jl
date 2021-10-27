9adb6cef890d2f00556cb3b2f10954f1688114cb
Documenter.jl <documenter@juliadocs.github.io>
2021-10-27 15:51:07 +0000

    build based on dba68c4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PlutoHTML","page":"PlutoHTML","title":"PlutoHTML","text":"","category":"section"},{"location":"","page":"PlutoHTML","title":"PlutoHTML","text":"Hi.","category":"page"}]
+}

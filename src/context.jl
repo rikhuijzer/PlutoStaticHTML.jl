f4d300a8f619030dba5b2a2a898fe26e4da60c82
Rik Huijzer <rikhuijzer@pm.me>
2022-02-17 08:38:33 +0100

    Add error when context is missing (#75)

--- src/context.jl
+++ src/context.jl
@@ -4,7 +4,15 @@
 Return the direct dependencies for a `notebook`.
 """
 function _direct_dependencies(notebook::Notebook)::String
-    deps = [last(pair) for pair in dependencies(notebook.nbpkg_ctx)]
+    ctx = notebook.nbpkg_ctx
+    if isnothing(ctx)
+        error("""
+            Failed to determine the notebook dependencies from the state of Pluto's built-in package manager
+            This can be fixed by setting `append_build_context=false`.
+            See https://github.com/rikhuijzer/PlutoStaticHTML.jl/issues/74 for more information and open an issue if the problem persists.
+            """)
+    end
+    deps = [last(pair) for pair in dependencies(ctx)]
     filter!(p -> p.is_direct_dep, deps)
     # Ignore stdlib modules.
     filter!(p -> !isnothing(p.version), deps)

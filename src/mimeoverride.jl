8fb5eb05ca3cac49039ef12f2ea0c78867ceaad5
Rik Huijzer <github@huijzer.xyz>
2023-10-22 19:18:19 +0200

    And how about this? String with quote inside

--- src/mimeoverride.jl
+++ src/mimeoverride.jl
@@ -1,7 +1,9 @@
-const CONFIG_PLUTORUNNER = quote
-    PlutoRunner.is_mime_enabled(::MIME"application/vnd.pluto.tree+object") = false
-    PlutoRunner.PRETTY_STACKTRACES[] = false
-end
+const CONFIG_PLUTORUNNER = """
+    quote
+        PlutoRunner.is_mime_enabled(::MIME"application/vnd.pluto.tree+object") = false
+        PlutoRunner.PRETTY_STACKTRACES[] = false
+    end
+    """
 
 # These overrides are used when `use_distributed=false`.
 eval(CONFIG_PLUTORUNNER)

05dccb59632987de6b8c84d16cb591b0c0e42316
Rik Huijzer <github@huijzer.xyz>
2023-10-22 18:06:55 +0200

    Remove TimerOutputs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,37 @@
 include("preliminaries.jl")
 
-const TIMEROUTPUT = TimerOutput()
-
-@timed_testset "pdf" begin
+@testset "pdf" begin
     include("pdf.jl")
 end
 
-@timed_testset "context" begin
+@testset "context" begin
     include("context.jl")
 end
 
-@timed_testset "cache" begin
+@testset "cache" begin
     include("cache.jl")
 end
 
-@timed_testset "mimeoverride" begin
+@testset "mimeoverride" begin
     include("mimeoverride.jl")
 end
 
-@timed_testset "with_terminal" begin
+@testset "with_terminal" begin
     include("with_terminal.jl")
 end
 
-@timed_testset "html" begin
+@testset "html" begin
     include("html.jl")
 end
 
-@timed_testset "style" begin
+@testset "style" begin
     include("style.jl")
 end
 
-@timed_testset "build" begin
+@testset "build" begin
     include("build.jl")
 end
 
-@timed_testset "aqua" begin
+@testset "aqua" begin
     Aqua.test_all(PlutoStaticHTML; ambiguities=false)
 end
-
-show(TIMEROUTPUT; compact=true, sortby=:firstexec)
